2eacb95f8709d3b23e95e6a5e8d2b9ff2b94def1
Melk12345 <105999204+Melk12345@users.noreply.github.com>
2022-06-08 10:48:42 -0700

    Add files via upload

--- index.js
+++ index.js
@@ -1,53 +1,25 @@
-const Discord = require("discord.js");
-const fs = require("fs");
-const path = require("path");
+import DiscordJS, { Intents, Message } from 'discord.js';
+import dotenv from 'dotenv';
+dotenv.config();
 
-const config = require("./config.json");
-
-const client = new Discord.Client();
-
-client.login(config.token);
-
-client.on("ready", () => {
-     // collection of commands that your bot will recognize
-     client.commands = new Discord.Collection();
-     // /source/ directory
-     let srcDir = path.resolve(__dirname, "./source");
-     // use file system (fs) to load each directory
-     let modules = fs.readdirSync(srcDir).filter(file => fs.statSync(
-         path.join(srcDir, file)).isDirectory());
- 
-     // loop through each directory and
-     modules.forEach(module=>{
-         // tell us where it's looking
-         console.log("Loading Folder: " + module);
- 
-         // find the js files
-         let commandFiles = fs.readdirSync(path.resolve(`${srcDir}/${module}`)).
-             filter(file => !fs.statSync(path.resolve(srcDir, module, file)).isDirectory()).
-             filter(file => file.endsWith('.js'));
- 
-         // loop through each js file in the directory
-         commandFiles.forEach((f, i) => {
-             // get the file
-             var cmds = require(`${srcDir}/${module}/${f}`);
-             // if it has a config
-             if (cmds.config) {
-                 // print it out and
-                 console.log(` =>Loaded file ${module}/${f}`);
-                 // add it to our collection of commands
-                 client.commands.set(cmds.config.command, cmds);
-             }
-         });
-     });
-    console.log("The bot is online.");
+const client = new DiscordJS.Client({
+    intents: [
+        Intents.FLAGS.GUILDS,
+        Intents.FLAGS.GUILD_MESSAGES
+    ]
 });
 
-// Every message we get runs through here
-client.on("message", msg => {
-    if (!msg.content.startsWith("!")) return;
-    let cont = msg.content.slice(1).split(" ");
-    let args = cont.slice(1);
-    let cmd = client.commands.get(cont[0].toLowerCase());
-    if (cmd) cmd.run(client, msg, args);
-});
+client.on('ready', () => {
+    console.log('The bot is online');
+})
+
+client.on('messageCreate', () => {
+    if (Message.content === 'ping') {
+        Message.reply({
+            content: 'pong',
+        })
+    }
+})
+
+client.login(process.env.TOKEN);
+
